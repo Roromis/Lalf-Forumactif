c6e38c45615ae5e61dd989f26eb799a543c23038
Roromis <admin@roromis.fr.nf>
2011-11-20 12:08:23 +0100

    Ajout du lanceur

---
+++ launcher.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+import sys, subprocess, time
+
+cont = 1
+out = ""
+cout = 0
+
+while cont:
+	time.sleep(10)
+	
+	p = subprocess.Popen([sys.executable, './forumactif-phpbb.py'])
+	p.wait()
+	cont = p.returncode
+	
+	if out == p.stdout:
+		cout += 1
+	else:
+		out = p.stdout
+		cout = 0
+	
+	if cout >= 10:
+		cont = 0
+		print "Le retour du script n'as pas changé lors des dix dernières exécutions."
+
+raw_input("Appuyez sur Entrée pour quitter...")
