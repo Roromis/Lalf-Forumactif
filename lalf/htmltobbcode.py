c0878a7b0947223248dcbec8d0ea54f35e6e8807
Lucas Seguinot <lucas.seguinot@gmail.com>
2014-04-29 22:58:49 +0200

    License

--- lalf/htmltobbcode.py
+++ lalf/htmltobbcode.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#       htmltobbcode.py
-#       
-#       Copyright 2010 Roromis <admin@roromis.fr.nf>
-#       
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
+# This file is part of Lalf.
+#
+# Lalf is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lalf.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
 from html.parser import HTMLParser
